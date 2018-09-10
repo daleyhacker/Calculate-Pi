13046ac63eaa7c97c74ab35b430abd9545afd3a2
daleyhacker <42852983+daleyhacker@users.noreply.github.com>
2018-09-10 07:52:21 -0400

    Updated from Brython Server: 9/10/2018 7:52:21 AM

--- calculatepi.py
+++ calculatepi.py
@@ -26,14 +26,3 @@ n = int(input("I will estimate pi. How many terms should I use? "))
 decimals = int(input("How many decimal places should I use in the result? "))
 pi = 4*sum([((-1.0)**k)/math.factorial(2*k+1) for k in range(0,n)])
 print("The approximate value of value pi is {0}".format(round(pi, decimals)))
-
-#print("(The true value of pi is {0})".format(round(math.pi, decimals)))
-
-"""
-import math
-n = int(input("I will estimate e. How many terms should I use? "))
-decimals = int(input("How many decimal places should I use in the result? "))
-e = 1.0/sum([((-1.0)**k)/math.factorial(k) for k in range(0,n)])
-print("The approximate value of e is {0}".format(round(e, decimals)))
-print("(The true value of e is {0})".format(round(math.e, decimals)))
-"""
